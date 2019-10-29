0b9fecb6b67b618fc3ea0f4d2ce567435e1f1815
Besir <udemiyhesap@gmail.com>
2019-10-29 13:32:42 +0300

    Add project files.

---
+++ WebServisDemo/CalcoaltiorWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="CalcoaltiorWebService.asmx.cs" Class="WebServisDemo.CalcoaltiorWebService" %>
---
+++ WebServisDemo/CalcoaltiorWebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServisDemo
+{
+    /// <summary>
+    /// Summary description for CalcoaltiorWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalcoaltiorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string Toplamaservisi(int valuone, int valoscond ,string secondersi)
+        {
+            return valuone + valoscond+secondersi;
+        }
+    }
+}
